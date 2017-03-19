f5b81392ca91eb26b1decc3fa421d2314218bc32
Caleb Porzio <calebporzio@gmail.com>
2017-03-19 19:35:11 -0400

    initial commit

---
+++ source/index.blade.php
@@ -0,0 +1,14 @@
+@extends('_layouts.master')
+
+@section('body')
+<div class="brand">
+    <h1 class="c-ye">LaravelPoetry;</h1>
+    <h4>
+        <span class="c-lb">{{'<'}}{{'?'}}{{'p'}}{{'h'}}{{'p '}}</span>
+            <span class="c-bl">ucwords</span><span class="c-lb">($</span><span class="c-wh">iSeePoetry</span><span class="c-lb">);</span>
+        <span class="c-lb">{{' ?'}}{{'>'}}</span>
+    </h4>
+    <h4 class="c-gra">/** ------------------------------------ &nbsp;&nbsp;&nbsp;</h4>
+    <h4 class="c-gra">&nbsp;&nbsp;&nbsp; ------------------------------------ **/</h4>
+</div>
+@endsection
