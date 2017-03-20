20d5c49d4a80eb77e5ce8fced46bcffe7a21f955
Caleb Porzio <calebporzio@gmail.com>
2017-03-20 08:23:25 -0400

    update some poems

--- source/index.blade.php
+++ source/index.blade.php
@@ -35,7 +35,7 @@
         <span class="c-lb">!</span> <span class="c-bl">count</span><span class="c-lb">($</span>chickens<span class="c-lb">);</span> <span class="c-bl">md5</span><span class="c-lb">($</span>chickens<span class="c-lb">)</span>
     @endcomponent
     @component('poem')
-        <span class="c-bl">date</span><span class="c-lb">('</span><span class="c-gr">My dAUghtr</span><span class="c-lb">') &&</span> <span class="c-bl">die</span><span class="c-lb">()</span>
+        <span class="c-lb">!</span> <span class="c-bl">date</span><span class="c-lb">('</span><span class="c-gr">My dAUghtr</span><span class="c-lb">')
     @endcomponent
     @component('poem')
         <span class="c-bl">mail</span><span class="c-lb">();</span> <span class="c-bl">time</span><span class="c-lb">();</span> <span class="c-bl">mail</span><span class="c-lb">();</span> <span class="c-bl">time</span><span class="c-lb">();</span> <span class="c-bl">mail</span><span class="c-lb">();</span> <span class="c-bl">time</span><span class="c-lb">()</span>
@@ -44,7 +44,7 @@
         <span class="c-pu">try</span> <span class="c-lb">{ $</span>_GET<span class="c-lb">[</span><span class="c-gr">'rich'</span><span class="c-lb">] }</span> <span class="c-pu">catch</span> <span class="c-lb">{</span> <span class="c-bl">die</span><span class="c-lb">(); }</span>
     @endcomponent
     @component('poem')
-        <span class="c-pu">if</span> <span class="c-lb">(</span><span class="c-bl">round</span><span class="c-lb">(</span><span class="c-gr">''</span><span class="c-lb">)) {</span> <span class="c-bl">trim</span><span class="c-lb">(</span>$fat<span class="c-lb">); }</span>
+        <span class="c-pu">if</span> <span class="c-lb">(</span><span class="c-bl">round</span><span class="c-lb">(</span><span class="c-gr">''</span><span class="c-lb">)) {</span> <span class="c-bl">trim</span><span class="c-lb">(</span>$_COOKIE<span class="c-lb">[</span><span class="c-bl">time</span><span class="c-lb">()]); }</span>
     @endcomponent
     @component('poem')
         <span class="c-or">__CLASS__</span> <span class="c-bl">eval</span><span class="c-lb">(</span><span class="c-gr">'entines'</span><span class="c-lb"> . </span><span class="c-bl">date</span><span class="c-lb">())</span>
