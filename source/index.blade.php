2c08cdc0b1f07c4f57089a057c57dba4fc332c6e
Caleb Porzio <calebporzio@gmail.com>
2017-03-19 21:38:11 -0400

    First Draft

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,13 +2,58 @@
 
 @section('body')
 <div class="brand">
-    <h1 class="c-ye">LaravelPoetry;</h1>
+    <h1 class="c-ye">PsrPoetry;</h1>
     <h4>
-        <span class="c-lb">{{'<'}}{{'?'}}{{'p'}}{{'h'}}{{'p '}}</span>
-            <span class="c-bl">ucwords</span><span class="c-lb">($</span><span class="c-wh">iSeePoetry</span><span class="c-lb">);</span>
-        <span class="c-lb">{{' ?'}}{{'>'}}</span>
+        @component('poem')
+            <span class="c-bl">ucwords</span><span class="c-lb">($</span><span class="c-wh">iSeePoetry</span><span class="c-lb">)</span>
+        @endcomponent
     </h4>
-    <h4 class="c-gra">/** ------------------------------------ &nbsp;&nbsp;&nbsp;</h4>
-    <h4 class="c-gra">&nbsp;&nbsp;&nbsp; ------------------------------------ **/</h4>
+    <p class="c-gra">// --------------------------------------------------------------------</p>
+
+    @component('poem')
+        <span class="c-pu">use</span> <span class="c-ye">ThingsAndLovePeople</span><span class="c-lb">; ! $</span>otherWayAround
+    @endcomponent
+    @component('poem')
+        <span class="c-pu">return</span> <span class="c-lb">$</span>favors
+    @endcomponent
+    @component('poem')
+        <span class="c-bl">count</span><span class="c-lb">($</span>blessings<span class="c-lb">)</span>
+    @endcomponent
+    @component('poem')
+        <span class="c-bl">time</span><span class="c-lb">();</span> <span class="c-bl">time</span><span class="c-lb">()</span>
+    @endcomponent
+    @component('poem')
+        <span class="c-bl">public</span> <span class="c-pu">function </span> __call<span class="c-lb">(</span><span class="c-gr">'it a'</span><span class="c-lb">) { </span><span class="c-bl">date</span><span class="c-lb">(<span class="c-gr">'D'</span><span class="c-lb">); }</span>
+    @endcomponent
+    @component('poem')
+        <span class="c-lb">$</span>injury <span class="c-lb">+= $</span>insult
+    @endcomponent
+    @component('poem')
+        <span class="c-lb">$</span>apples <span class="c-lb">=== $</span>oranges
+    @endcomponent
+    @component('poem')
+        <span class="c-lb">!</span> <span class="c-bl">count</span><span class="c-lb">($</span>chickens<span class="c-lb">);</span> <span class="c-bl">md5</span><span class="c-lb">($</span>chickens<span class="c-lb">)</span>
+    @endcomponent
+    @component('poem')
+        <span class="c-bl">date</span><span class="c-lb">('</span><span class="c-gr">My dAUghtr</span><span class="c-lb">') &&</span> <span class="c-bl">die</span><span class="c-lb">()</span>
+    @endcomponent
+    @component('poem')
+        <span class="c-bl">mail</span><span class="c-lb">();</span> <span class="c-bl">time</span><span class="c-lb">();</span> <span class="c-bl">mail</span><span class="c-lb">();</span> <span class="c-bl">time</span><span class="c-lb">();</span> <span class="c-bl">mail</span><span class="c-lb">();</span> <span class="c-bl">time</span><span class="c-lb">()</span>
+    @endcomponent
+    @component('poem')
+        <span class="c-pu">try</span> <span class="c-lb">{ $</span>_GET<span class="c-lb">[</span><span class="c-gr">'rich'</span><span class="c-lb">] }</span> <span class="c-pu">catch</span> <span class="c-lb">{</span> <span class="c-bl">die</span><span class="c-lb">(); }</span>
+    @endcomponent
+    @component('poem')
+        <span class="c-pu">if</span> <span class="c-lb">(</span><span class="c-bl">round</span><span class="c-lb">(</span><span class="c-gr">''</span><span class="c-lb">)) {</span> <span class="c-bl">trim</span><span class="c-lb">(</span>$fat<span class="c-lb">); }</span>
+    @endcomponent
+    @component('poem')
+        <span class="c-or">__CLASS__</span> <span class="c-bl">eval</span><span class="c-lb">(</span><span class="c-gr">'entines'</span><span class="c-lb"> . </span><span class="c-bl">date</span><span class="c-lb">())</span>
+    @endcomponent
+
+    <h4 class="c-gra">
+        //--------------//<br>
+        <a href="mailto:calebporzio@gmail.com">Submit One</a><br>
+        //--------------//&nbsp;&nbsp;
+    </h4>
 </div>
 @endsection
---
+++ source/poem.blade.php
@@ -0,0 +1,4 @@
+<p>
+    <span class="c-lb">{{'<'}}{{'?'}}{{'p'}}{{'h'}}{{'p '}}</span>
+        {{ $slot }}<span class="c-lb">{{'; ?'}}{{'>'}}</span>
+</p>
