5f9645fde404185ab8ca7ee5dabcb37b2615bcf8
Caleb Porzio <calebporzio@gmail.com>
2017-03-20 08:33:00 -0400

    add favicon and build

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,6 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
         <link rel="stylesheet" href="/css/main.css">
     </head>
     <body>
